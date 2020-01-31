6b12093e6ddddb9445b022a80a982a2d09f712c6
Colin Murphy <36647057+colinmurphy42@users.noreply.github.com>
2020-01-31 15:45:19 -0700

    Update Cat.java

--- OOProject1/Cat.java
+++ OOProject1/Cat.java
@@ -1,10 +1,12 @@
 public class Cat extends Feline{
     public String name = "";
+    //The name parameter is set as the animals name
     public Cat(String givenName){
         this.name=givenName;
     }
     
-    	public String makeNoise(){
+   //The sleep function is where we randomly select a method, so all other functions call sleep
+   public String makeNoise(){
       return sleep();
    }
    public String eat(){
@@ -22,7 +24,7 @@ public class Cat extends Feline{
     public String getname(){
       return(name);
     }
-    
+    //Using Math.random to get random numbers, and then mapping those numbers to methods
   	public String sleep(){
       int y = (int)(Math.random() * ((5 - 1) + 1)) + 1;
       
