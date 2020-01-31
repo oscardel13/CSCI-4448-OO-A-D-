cbc67693ff1c7fe78663bf78ef60c41186fe6978
Colin Murphy <36647057+colinmurphy42@users.noreply.github.com>
2020-01-31 15:35:05 -0700

    Update Main.java

--- OOProject1/Main.java
+++ OOProject1/Main.java
@@ -1,7 +1,8 @@
 public class Main {
   
-    public static void main(String[] args) {
-	Cat colin = new Cat("Colin"); 
+  public static void main(String[] args) {
+  //Here we create all the animal objects and pass in their names
+  Cat colin = new Cat("Colin"); 
   Cat cashmere = new Cat("Cashmere");
   Dog donnie = new Dog("Donnie");
   Dog dion = new Dog("Dion");
@@ -18,6 +19,7 @@ public class Main {
   Elephant egg = new Elephant ("Egg");
   Elephant ebon = new Elephant ("Ebon");
   Zookeeper bob = new Zookeeper();
+  //Then we put all of the objects in a list
   Animal[] zoo = {colin,cashmere,donnie,dion,wumpy,wanda,tony,tinderthot,lary,loscar,hilda,hippona,rexy,rastaman,egg,ebon};
   
   //System.out.println(zoo.animals);
@@ -25,6 +27,8 @@ public class Main {
   
   //System.out.println(colin.roam());
   //System.out.println(yo[0].name);
+	  
+  //Then we call on the Zookeeper to do all his things
   Zookeeper.wakeUp(zoo);
   Zookeeper.rollCall(zoo);
   Zookeeper.feed(zoo);
