698111c7787388deb57061e542ef9c89ff2a7403
Oscar Delgado <oscardel413@gmail.com>
2020-02-12 21:36:29 -0700

    All the work done!

---
+++ Project2/Animal.py
@@ -0,0 +1,22 @@
+class Animal:
+    Roam = ""
+    Name = ""
+    Type = ""
+    def __init__():
+        return
+    def sleep():
+        return "Goes to bed"
+    def eat():
+        return "Eats"
+    def wake():
+        return "Wakes up"
+    def performRoam():
+        return
+    def setRoamBehavior(self):
+        self.Roam = "whatever"
+
+def main():
+    print ("Hello World!")
+
+if __name__ == "__main__":
+    main
