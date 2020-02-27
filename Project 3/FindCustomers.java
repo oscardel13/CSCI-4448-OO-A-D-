c2bc5a653c205c49333e8327aff2d7d5592afc57
Colin Murphy <comu0011@colorado.edu>
2020-02-26 23:36:35 -0700

    Started FindCustomers

---
+++ Project 3/FindCustomers.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+//This class decides what customers come into the store that day
+public class FindCustomers extends CustomerList{
+    public FindCustomers(ArrayList<Customer> cL){
+        customerList = cL;
+    }
+
+}
